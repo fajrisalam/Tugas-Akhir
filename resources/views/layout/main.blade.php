a7a1f35c8ebac046a5200049e62fc5c149e3ac09
fajrisalam <fajrisalam289@gmail.com>
2019-05-18 01:19:02 +0700

    templating admin bsb

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>@yield('title')</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    @include('layout.css')
+</head>
+
+<body class="theme-red">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Please wait...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <!-- Search Bar -->
+    <div class="search-bar">
+        <div class="search-icon">
+            <i class="material-icons">search</i>
+        </div>
+        <input type="text" placeholder="START TYPING...">
+        <div class="close-search">
+            <i class="material-icons">close</i>
+        </div>
+    </div>
+    <!-- #END# Search Bar -->
+    <!-- Top Bar -->
+    @include('layout.navbar')
+    <!-- #Top Bar -->
+    <section>
+        <!-- Left Sidebar -->
+        @include('layout.left-sidebar')
+        <!-- #END# Left Sidebar -->
+        <!-- Right Sidebar -->
+        @include('layout.right-sidebar')
+        <!-- #END# Right Sidebar -->
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>@yield('content-title')</h2>
+            </div>
+        </div>
+        @yield('content')
+    </section>
+
+    @include('layout.js')
+</body>
+
+</html>
---
+++ resources/views/testing/testo.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+
+@section('title', 'cobacobagan')
+
+@section('content-title', 'ini title dude')
+@section('content')
+hehehe
+@stop
