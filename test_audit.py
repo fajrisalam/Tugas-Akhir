637356c9d1cf64ff8dd28a5f8d960ff44729bb7a
Alvin Mudhoffar <alvinmudhoffar@gmail.com>
2019-07-11 19:59:00 +0700

    penetapan flag

---
+++ test_audit.py
@@ -0,0 +1,52 @@
+import os
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+import datetime;
+try:
+   mySQLconnection = mysql.connector.connect(host='178.128.80.206',database='skripsi',user='buaya',password='sembarang12')
+   sql_select_Query = "select * from files"
+   cursor = mySQLconnection .cursor()
+   cursor.execute(sql_select_Query)
+   records = cursor.fetchall()
+   for row in records:
+     file = 'storage\\app\\' + row[2]
+     sha = row[9]
+
+     is_exist = os.path.isfile(file)
+     print(is_exist)
+     # if file tidak ada
+    #  if(is_exist == False):
+    #   update = "update files set `delete` = 1 where id = " + str(row[0])
+    #   cursor.execute(update)
+    #   ts = datetime.datetime.now().timestamp()
+    #   time = datetime.datetime.fromtimestamp(ts).isoformat()
+    #   new_time = time.replace('T', ' ')
+    #   time = new_time[:19]
+    #   insert = "insert into logs (`user_id`, `file_id`, `execution`, `duration`, `created_at`) values (0, "+str(row[0])+", 4, '-', '" + str(time)+"')"
+    #   cursor.execute(insert)
+    #   mySQLconnection.commit()
+#      # if file ada
+#      else:
+#        checksum = "sha256sum " + file
+#        process = os.popen(checksum).read()
+#        cc = process.split()[0]
+#        if(cc[1:] != sha):
+#         update = "update files set modif = 1 where id = " + str(row[0])
+#         cursor.execute(update)
+#         ts = datetime.datetime.now().timestamp()
+#         time = datetime.datetime.fromtimestamp(ts).isoformat()
+#         new_time = time.replace('T', ' ')
+#         time = new_time[:19]
+#         insert = "insert into logs (`user_id`, `file_id`, `execution`, `duration`, `created_at`) values (0, "+str(row[0])+", 5, '-', '" + str(time)+"')"
+#         cursor.execute(insert)
+#         # file belum dihapus ingat ini
+#         mySQLconnection.commit()
+#    cursor.close()  
+# except Error as e :
+#     print ("Error while connecting to MySQL", e)
+# finally:
+#     #closing database connection.
+#     if(mySQLconnection .is_connected()):
+#         mySQLconnection.close()
+#         print("MySQL connection is closed")
